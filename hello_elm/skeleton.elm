c50b255028afea21e4079a869cee543c7041d0c7
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-08-27 23:14:56 -0500

    Explanation in architecture

--- hello_elm/skeleton.elm
+++ hello_elm/skeleton.elm
@@ -1,5 +1,11 @@
 import Html exposing (..)
 
+-- The Elm Architecture is built by three components:
+-- The model - the data | The Update - the way for transforming
+-- your data | The View - Shows your data through HTML
+-- The following code can serve as an Skeleton for the majority of
+-- Elm projects
+
 -- Model 
 
 type alias Model = { ... }
