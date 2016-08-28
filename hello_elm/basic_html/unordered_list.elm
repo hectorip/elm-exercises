f1869373596483f726824dd287f39e8d555f8187
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-08-27 23:55:48 -0500

    Notes - using spanish in notes

--- hello_elm/basic_html/unordered_list.elm
+++ hello_elm/basic_html/unordered_list.elm
@@ -1,6 +1,9 @@
 import Html exposing (li, text, ul)
 import Html.Attributes exposing (class)
 
+-- Cada Elemento HTML recibe dos listas:
+-- 1) atributos HTML que necesite
+-- 2) elementos que van adentro de la etiqueta
 main =
     ul [class "components-list"]
         [ li [] [text "Model"]
