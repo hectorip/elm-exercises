a01d4b03a13464b4617a6445c277d2d6e339d3ec
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-08-29 23:59:16 -0500

    Reset button

--- hello_elm/counter.elm
+++ hello_elm/counter.elm
@@ -5,7 +5,7 @@ import Html.Events exposing (onClick)
 main =
   App.beginnerProgram { model = 0, view = view, update = update }
 
-type Msg = Increment | Decrement
+type Msg = Increment | Decrement | Reset
 
 update msg model =
   case msg of
@@ -15,9 +15,13 @@ update msg model =
     Decrement ->
       model - 1
 
+    Reset ->
+      model = 0
+
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+"]
+    , button [onClick Reset] [text "to Zero"]
     ]
