bf4698aaf850517e2f03f9e05b4ededef7e58dc3
Hector Ivan Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-09-03 23:42:03 -0500

    Fixing errors

--- hello_elm/user_input/buttons.elm
+++ hello_elm/user_input/buttons.elm
@@ -3,12 +3,12 @@ import Html.App exposing (beginnerProgram)
 import Html.Events exposing (onClick)
 
 main =
-    beginnerProgram { model = 0, view = view, update = update }
+    beginnerProgram { model = "", view = view, update = update }
 
 view model = 
     div []
-        [ button [onclick SayHello ] [ text "Say Hello!" ]
-        , button [onclick SayGoodBye ] [ text "Say Good bye!" ]
+        [ button [onClick SayHello ] [ text "Say Hello!" ]
+        , button [onClick SayGoodBye ] [ text "Say Good bye!" ]
         , div [] [ text (toString model) ]
         ]
 
